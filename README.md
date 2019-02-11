501d043143810be1f44feebf2cdcf836db54592a
Peter Huang <peter.huang@enflick.com>
2019-02-11 12:17:54 -0500

    sample images update for file io

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ You can use gradle clean, gradle build, gradle run on this project
 AppScreen is the main JPanel for the whole EasyPaint application, and the sub panels are divided
 into its respective classes within the Main.Screens folder. Each drawn shape on the screen is also
 an object, and each of the objects are defined in its respective class in Main.CanvasShapes.
+
+To load files, sample image .ezp (Easy Paint files) are located in the 'image files' folder, within the 
+same level as src, readme, and build. You can save your images in another folder but the sample 
+images will always be under image files!
--- src/Main/EasyPaint.java
+++ src/Main/EasyPaint.java
@@ -9,13 +9,16 @@ import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
 import java.awt.event.*;
+import java.awt.Color;
 import java.awt.Desktop;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.awt.Point;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 import Main.AppScreen;
+import Main.CanvasShapes.*;
 import Main.Screens.CanvasScreen;
 
 public class EasyPaint extends JFrame implements ActionListener {
@@ -79,26 +82,130 @@ public class EasyPaint extends JFrame implements ActionListener {
 			JFileChooser jfc = new JFileChooser();
     		FileNameExtensionFilter filter = new FileNameExtensionFilter("EasyPaint Images", "ezp");
 			jfc.setFileFilter(filter);
-			int returnVal = jfc.showOpenDialog(this);
-			if(returnVal == JFileChooser.APPROVE_OPTION) {
+			int state = jfc.showOpenDialog(this);
+			File myFile = jfc.getSelectedFile();
+			if(myFile != null && state == JFileChooser.APPROVE_OPTION) {
 				System.out.println("You are opening the file: " + jfc.getSelectedFile().getName());
+
+				List<CanvasShape> tempList = CanvasScreen.shapesList;
+				BufferedReader reader;
+				try {
+					reader = new BufferedReader(new FileReader(myFile));
+					String line = reader.readLine(); //skip line 1
+					line = reader.readLine();
+
+					List<CanvasShape> newShapesList = new ArrayList<CanvasShape>();
+					while (true) {
+						line = reader.readLine();
+						if (line.trim().equals("end")) break;
+
+						String [] components = line.split(Pattern.quote("|"));
+						int compLen = components.length;
+						for (int j = 0; j < compLen; j++) {
+							components[j] = components[j].trim();
+						}
+
+						String shapeType = components[0];
+						int startX = Integer.parseInt(components[1]);
+						int startY = Integer.parseInt(components[2]);
+						int endX = Integer.parseInt(components[3]);
+						int endY = Integer.parseInt(components[4]);
+						int myWidth = Integer.parseInt(components[5]);
+						int myHeight = Integer.parseInt(components[6]);
+						int myThicc = Integer.parseInt(components[7]);
+						Color myColour = new Color(Integer.parseInt(components[8]));
+						boolean isFilledIn = false;
+						if (components[9].equalsIgnoreCase("true")) isFilledIn = true;
+
+						Point startPt = new Point(startX, startY);
+						Point endPt = new Point(endX, endY);
+
+						CanvasShape currentShape;// = new CurrentShape();
+						if (shapeType.equals("CanvasLine")) {
+							currentShape = new CanvasLine(startPt, endPt, myThicc, myColour);
+							currentShape.isFilled = false;
+							newShapesList.add(currentShape);
+						} else if (shapeType.equals("CanvasOval")) {
+							currentShape = new CanvasOval(startX, startY, myWidth, myHeight, myThicc, myColour);
+							currentShape.isFilled = isFilledIn;
+							newShapesList.add(currentShape);
+						} else if (shapeType.equals("CanvasRectangle")) {
+							currentShape = new CanvasRectangle(startX, startY, myWidth, myHeight, myThicc, myColour);
+							currentShape.isFilled = isFilledIn;
+							newShapesList.add(currentShape);
+						}
+						//don't add here because we have them all added above
+					}
+					CanvasScreen.shapesList.clear();
+					CanvasScreen.shapesList.addAll(newShapesList);
+
+					reader.close();
+
+				} catch (IOException e) {
+					e.printStackTrace();
+					CanvasScreen.shapesList = tempList;
+				} catch (ArrayIndexOutOfBoundsException e2) {
+					e2.printStackTrace();
+					CanvasScreen.shapesList = tempList;
+				} catch (NumberFormatException e3) {
+					e3.printStackTrace();
+					CanvasScreen.shapesList = tempList;
+				} catch (Exception e4) {
+					//other exceptions
+					e4.printStackTrace();
+					CanvasScreen.shapesList = tempList;
+				}
+
 			}
 
 			System.out.println("Load to be implemented");
+			g.drawingPanel.repaint();
+			g.drawingPanel.repaint();
 		} else if (ae.getActionCommand().equals("Save")) {
 			System.out.println("Save to be implemented");
 
 			JFileChooser jfc = new JFileChooser();
 			FileNameExtensionFilter filter = new FileNameExtensionFilter("EasyPaint Images", "ezp");
 			jfc.setFileFilter(filter);
-			int returnVal = jfc.showSaveDialog(this);
-			if(returnVal == JFileChooser.APPROVE_OPTION) {
-				System.out.println("You are saving the file: " + jfc.getSelectedFile().getName());
-			
-				PrintWriter printOut;
+			int state = jfc.showSaveDialog(this);
+			File myFile = new File(jfc.getSelectedFile() + ".ezp");
+			//File myFile = new File(jfc.getSelectedFile() + ".txt");
+			if (myFile != null) myFile.delete(); //overwrite
+			if(state == JFileChooser.APPROVE_OPTION) {
+				//System.out.println(jfc.getSelectedFile().getAbsolutePath());
+				PrintWriter filewriter;
 				try {
-					printOut = new PrintWriter(new FileWriter("sampleFile.txt"));
-					printOut.println();
+					BufferedWriter bfw = new BufferedWriter(new FileWriter(myFile));
+					filewriter = new PrintWriter(bfw);
+					filewriter.println("Easy Paint by Peter Huang");
+					//System.out.println("Reached printing to " + jfc.getSelectedFile().getName());
+
+					//int len = CanvasScreen.shapesList.size(); 
+					for (CanvasShape cshape : CanvasScreen.shapesList) {
+						String infoLine = "";
+						if (cshape instanceof CanvasLine) {
+							infoLine = "CanvasLine| ";
+						} else if (cshape instanceof CanvasOval) {
+							infoLine = "CanvasOval| ";
+						} else if (cshape instanceof CanvasRectangle) {
+							infoLine = "CanvasRectangle| ";
+						} else {
+							continue;
+						}
+
+						//startpoint(x,y), endpoint(x,y), width, height, thickness, colour, isFilled
+						String linePt2 = cshape.getStartPoint().x + "| " + cshape.getStartPoint().y + "| " +
+							cshape.getEndPoint().x + "| " + cshape.getEndPoint().y + "| " + cshape.getWidth() + "| " +
+							cshape.getHeight() + "| " +	cshape.getThickness() + "| " + cshape.getColour().getRGB() + 
+							"| " + cshape.isFilled;
+
+						//cshape.getColour().getRGB();
+
+						infoLine = infoLine.concat(linePt2);
+						filewriter.println(infoLine);
+					}
+					filewriter.println("end");
+					filewriter.close();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
